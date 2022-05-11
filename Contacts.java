a2700b6f4e7647beda445f67b7c06102dd03d662
rohitmartand123 <rohitmartand274@gmail.com>
2022-05-11 18:55:10 +0530

    Added Method to search by region

--- Contacts.java
+++ Contacts.java
@@ -7,98 +7,54 @@ package addressbookmanagementsystem;
  */
 
 public class Contacts {
-    // Variable
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String city;
-    private String state;
-    private String pinCode;
-    private String mobileNo;
-    private String email;
+	 private String firstName, lastName, email, mobileNumber;
+	    Address address;
 
-    // Parameterised Constructor
-    public Contacts(String firstName, String lastName, String address, String city, String state, String pincode,
-                    String mobileNo, String email) {
+	    // Getter and Setter Methods
+	    public String getFirstName() {
+	        return firstName;
+	    }
 
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.pinCode = pincode;
-        this.mobileNo = mobileNo;
-        this.email = email;
-    }
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
 
-    // Getter and Setter methods
-    public String getfirstName() {
-        return firstName;
-    }
+	    public String getLastName() {
+	        return lastName;
+	    }
 
-    public void setfirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	    public void setLastName(String lastName) {
+	        this.lastName = lastName;
+	    }
 
-    public String getlastName() {
-        return lastName;
-    }
+	    public String getEmail() {
+	        return email;
+	    }
 
-    public void setlastName(String lastName) {
-        this.lastName = lastName;
-    }
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
 
-    public String getAddress() {
-        return address;
-    }
+	    public String getmobileNumber() {
+	        return mobileNumber;
+	    }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	    public void setmobileNumber(String mobileNumber) {
+	        this.mobileNumber = mobileNumber;
+	    }
 
-    public String getCity() {
-        return city;
-    }
+	    public void setAddress(Address address) {
+	        this.address = address;
+	    }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	    public Address getAddress() {
+	        return address;
+	    }
 
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getPinCode() {
-        return pinCode;
-    }
-
-    public void setPinCode(String pinCode) {
-        this.pinCode = pinCode;
-    }
-
-    public String getMobileNo() {
-        return mobileNo;
-    }
-
-    public void setMobileNo(String mobileNo) {
-        this.mobileNo = mobileNo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String eMail) {
-        this.email = eMail;
-    }
-
-    @Override
-    public String toString() {
-        return "Contacts {" + "\n FirstName : " + firstName + ", LastName : " + lastName + " \n Address : " + address + ", City : " + city + ", State : " + state + "\n Mobile No : " + mobileNo + ", Pin-Code : " + pinCode + ", \n Email : " + email + '}';
+	    @Override
+	    public String toString() {
+	        return "Contact [First Name=" + firstName + ", Last Name=" + lastName + ", Email=" + email + ", Mobile Number="
+	                + mobileNumber + " , " + address + "]";
 
     }
 }
